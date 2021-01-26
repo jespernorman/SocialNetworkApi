4179ce7237d1d69640f3417e8e1d2d2a149231fa
sickshit <sickshit>
2021-01-26 20:23:46 +0100

    commit

---
+++ socialnetworkapi/Like.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialNetworkApi
+{
+    public class Like
+    {
+        public int LikeCounter { get; set; }
+
+        public List<int> ListOfLikes = new List<int>();
+
+        public Like()
+        {
+
+        }
+
+        public void AddLike(int postItemId, int userId, string dbPath)
+        {
+            ListOfLikes.Add(postItemId);
+        }
+
+        public void blBSLBSS ()
+        {
+
+        }
+    }
+}
